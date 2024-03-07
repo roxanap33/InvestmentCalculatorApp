6364b4d945e18f9b793dfcb84076db4f1bb04eb8
roxanap33 <roxana.ciocian@principal33.com>
2024-03-07 10:43:58 +0200

    96: Create initial UserInput component and add it to the App component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
-  return <Header />;
+  return (
+    <main>
+      <Header />
+      <UserInput />
+    </main>
+  );
 }
 
 export default App;
---
+++ 01-starting-project/src/components/UserInput.jsx
@@ -0,0 +1,20 @@
+export default function UserInput() {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <div>
+          <label>Initial Investment</label>
+          <input></input>
+          <label>Expected Return</label>
+          <input></input>
+        </div>
+        <div>
+          <label>Annual Investment</label>
+          <input></input>
+          <label>Duration</label>
+          <input></input>
+        </div>
+      </div>
+    </section>
+  );
+}
