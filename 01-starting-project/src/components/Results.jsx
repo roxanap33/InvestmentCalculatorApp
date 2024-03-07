26273fff373cd539f63cd77c4c57d014d8d6d240
roxanap33 <roxana.ciocian@principal33.com>
2024-03-07 13:57:38 +0200

    96: Populate table with static data

--- 01-starting-project/src/components/Results.jsx
+++ 01-starting-project/src/components/Results.jsx
@@ -1,15 +1,80 @@
+const tableHeaders = [
+  "Year",
+  "Investment Value",
+  "Interest (Year)",
+  "Total Interest",
+  "Invested Capital",
+];
+
+const tableData = [
+  {
+    year: 2000,
+    investmentValue: 200,
+    interestYear: 100,
+    totalInterest: 4000,
+    investedCapital: 3000,
+  },
+  {
+    year: 2001,
+    investmentValue: 200,
+    interestYear: 100,
+    totalInterest: 4000,
+    investedCapital: 3000,
+  },
+  {
+    year: 2002,
+    investmentValue: 200,
+    interestYear: 100,
+    totalInterest: 4000,
+    investedCapital: 3000,
+  },
+  {
+    year: 2003,
+    investmentValue: 200,
+    interestYear: 100,
+    totalInterest: 4000,
+    investedCapital: 3000,
+  },
+  {
+    year: 2004,
+    investmentValue: 200,
+    interestYear: 100,
+    totalInterest: 4000,
+    investedCapital: 3000,
+  },
+  {
+    year: 2005,
+    investmentValue: 200,
+    interestYear: 100,
+    totalInterest: 4000,
+    investedCapital: 3000,
+  },
+];
+
+function getTableHeaders(tableHeaders) {
+  return tableHeaders.map((header) => <th>{header}</th>);
+}
+
 export default function Results() {
   return (
     <table id="result">
       <thead>
-        <tr>
-          <th>Year</th>
-          <th>Investment Value</th>
-          <th>Interest (Year)</th>
-          <th>Total Interest</th>
-          <th>Invested Capital</th>
-        </tr>
+        <tr>{getTableHeaders(tableHeaders)}</tr>
       </thead>
+      <tbody>
+        {tableData &&
+          tableData.map((row) => {
+            return (
+              <tr key={row.year}>
+                <td>{row.year}</td>
+                <td>{row.investmentValue}</td>
+                <td>{row.interestYear}</td>
+                <td>{row.totalInterest}</td>
+                <td>{row.investedCapital}</td>
+              </tr>
+            );
+          })}
+      </tbody>
     </table>
   );
 }
