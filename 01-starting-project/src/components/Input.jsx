9de2d5024b45b7e4fba5cbb9e4bc9e603c09428f
roxanap33 <roxana.ciocian@principal33.com>
2024-03-07 11:34:22 +0200

    96: Add function for changing the input values and rearrange form

--- 01-starting-project/src/components/Input.jsx
+++ 01-starting-project/src/components/Input.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 export default function Input({ labelText }) {
+  const [inputValue, setInputValue] = useState(0);
+
+  function handleValueChange(event) {
+    setInputValue(event.target.value);
+  }
+
   return (
-    <>
+    <div>
       <label>{labelText}</label>
-      <input></input>
-    </>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={handleValueChange}
+      ></input>
+    </div>
   );
 }
--- 01-starting-project/src/components/UserInputForm.jsx
+++ 01-starting-project/src/components/UserInputForm.jsx
@@ -4,14 +4,12 @@ export default function UserInput() {
   return (
     <section id="user-input">
       <div className="input-group">
-        <div>
-          <Input labelText="initial investment" />
-          <Input labelText="expected return" />
-        </div>
-        <div>
-          <Input labelText="annual investment" />
-          <Input labelText="duration" />
-        </div>
+        <Input labelText="initial investment" />
+        <Input labelText="annual investment" />
+      </div>
+      <div className="input-group">
+        <Input labelText="expected return" />
+        <Input labelText="duration" />
       </div>
     </section>
   );
